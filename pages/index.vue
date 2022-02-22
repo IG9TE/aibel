b14437e574c0e3596be7f4faa674d7979904c5d3
Theo Toft <miljoagenttheo@gmail.com>
2022-02-22 11:56:55 +0100

    made aibel

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="background">
+    <div class="backdrop">
+      <div class="item1"><h1 class="text">hello1</h1></div>
+      <div class="item2"><h1>hello2</h1></div>
+      <div class="item3"><h1>hello3</h1></div>
+    </div>
+  </div>
+</template>
