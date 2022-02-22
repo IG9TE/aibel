09f9c0e0bcfb3bea1612feb66ae95a515d7870db
Theo Toft <miljoagenttheo@gmail.com>
2022-02-22 14:39:55 +0100

    changed

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="background">
+    <div class="navbar">
+      <h1 class="navbarhome">Hjem</h1>
+    </div>
     <div class="backdrop">
       <div class="item1"><h1 class="text">hello1</h1></div>
       <div class="item2"><h1>hello2</h1></div>
