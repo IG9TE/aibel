dc65b80210afff1295da9210eff4f357bd9f49e2
Theo Toft <miljoagenttheo@gmail.com>
2022-02-22 14:48:14 +0100

    changed

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="background">
+  <div>
     <div class="navbar">
       <h1 class="navbarhome">Hjem</h1>
     </div>
-    <div class="backdrop">
-      <div class="item1"><h1 class="text">hello1</h1></div>
-      <div class="item2"><h1>hello2</h1></div>
-      <div class="item3"><h1>hello3</h1></div>
+    <div class="background">
+      <div class="backdrop">
+        <div class="item1"><h1 class="text">hello1</h1></div>
+        <div class="item2"><h1>hello2</h1></div>
+        <div class="item3"><h1>hello3</h1></div>
+      </div>
     </div>
   </div>
 </template>
